325fb48900807616d579529fa287cb00e5d4201f
Gareth Mensah <gmensah@gmail.com>
2015-11-11 00:10:14 -0800

    Quick Fix.

--- CelScoreWidget/TodayTableViewCell.swift
+++ CelScoreWidget/TodayTableViewCell.swift
@@ -1,5 +1,5 @@
 //
-//  CelebTableViewCell.swift
+//  TodayTableViewCell.swift
 //  CelScore
 //
 //  Created by Gareth.K.Mensah on 11/10/15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CelebTableViewCell: UITableViewCell {
+class TodayTableViewCell: UITableViewCell {
     
     //MARK: Properties
     @IBOutlet internal var nickNameLabel: UILabel!
--- CelScoreWidget/TodayViewController.swift
+++ CelScoreWidget/TodayViewController.swift
@@ -7,11 +7,26 @@
 //
 
 import UIKit
+import RealmSwift
 import NotificationCenter
 
 class TodayViewController: UITableViewController, NCWidgetProviding {
     
     //MARK: Properties
+    let expandButton = UIButton()
+    let userDefaults = NSUserDefaults.standardUserDefaults()
+    var expanded : Bool {
+        get {
+            return userDefaults.boolForKey("expanded")
+        }
+        set (newExpanded) {
+            userDefaults.setBool(newExpanded, forKey: "expanded")
+            userDefaults.synchronize()
+        }
+    }
+    
+    let defaultNumRows = 3
+    let maxNumberOfRows = 10
     
     
     //MARK: Methods
@@ -29,4 +44,55 @@ class TodayViewController: UITableViewController, NCWidgetProviding {
         completionHandler(NCUpdateResult.NewData)
     }
     
+    func updatePreferredContentSize() {
+        preferredContentSize = CGSizeMake(CGFloat(0), CGFloat(tableView(tableView, numberOfRowsInSection: 0)) * CGFloat(tableView.rowHeight) + tableView.sectionFooterHeight)
+    }
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        coordinator.animateAlongsideTransition({ context in
+            self.tableView.frame = CGRectMake(0, 0, size.width, size.height)
+            }, completion: nil)
+    }
+
+    
+    // MARK: Table view data source
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let items = items {
+            return min(items.count, expanded ? maxNumberOfRows : defaultNumRows)
+        }
+        return 0
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("CelebItem", forIndexPath: indexPath) as! TodayTableViewCell
+        
+        if let item = items?[indexPath.row] {
+            cell.nickNameLabel.text = item.title
+            cell.celscoreLabel.text = item.author
+        }
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if let item = items?[indexPath.row] {
+            if let context = extensionContext {
+                context.openURL(item.link, completionHandler: nil)
+            }
+        }
+    }
+    
+    override func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return expandButton
+    }
+    
+    
+    // MARK: expand
+    func updateExpandButtonTitle() { expandButton.setTitle(expanded ? "Show less" : "Show more", forState: .Normal) }
+    
+    func toggleExpand() {
+        expanded = !expanded
+        updateExpandButtonTitle()
+        updatePreferredContentSize()
+        tableView.reloadData()
+    }
 }
