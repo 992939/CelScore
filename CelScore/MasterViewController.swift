def287220fa56e55d9ca0855395894ce936df33b
Gareth Mensah <gmensah@gmail.com>
2015-11-17 18:05:54 -0800

    Quick Fix.

--- CelScore/MasterViewController.swift
+++ CelScore/MasterViewController.swift
@@ -93,12 +93,11 @@ final class MasterViewController: UIViewController, ASTableViewDataSource, ASTab
     func numberOfSectionsInTableView(tableView: UITableView!) -> Int { return 1 }
     func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int { return self.displayedCelebrityListVM.count }
     func tableView(tableView: ASTableView!, willDisplayNodeForRowAtIndexPath indexPath: NSIndexPath!) { /*TODO: Implement*/ }
+    
     func tableView(tableView: ASTableView!, nodeForRowAtIndexPath indexPath: NSIndexPath!) -> ASCellNode! {
         var node = ASCellNode()
         self.displayedCelebrityListVM.getCelebrityProfileSignal(index: indexPath.row)
-            .on(next: { value in
-                node = CelebrityTableViewCell(profile: value)
-            })
+            .on(next: { value in node = CelebrityTableViewCell(profile: value) })
             .start()
         return node
     }
