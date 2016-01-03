34fbbab715b8962cfdaeff3b619ff38fb88a5427
Gareth Mensah <gmensah@gmail.com>
2016-01-02 19:08:51 -0800

    Quick Fix.

--- CelScore/Constants.swift
+++ CelScore/Constants.swift
@@ -8,4 +8,59 @@
 
 import Foundation
 
-struct Constants { static let cognitoIdentityPoolId = "us-east-1:d08ddeeb-719b-4459-9a8f-91cb108a216c" }
+struct Constants {
+    static let cognitoIdentityPoolId = "us-east-1:d08ddeeb-719b-4459-9a8f-91cb108a216c"
+    
+    let forturneCookieSayings: [String] = [
+        "Voting is like breathing life into civic values.", // 0 - first time vote
+        "Without ethics, man has no future.",
+        "Kindness is the sunshine in which virtue grows.",
+        "Being a virtuous human being takes practice.",
+        "Hate cannot drive out hate, only love can do that.",
+        "A true man hates no one.",
+        "To err is human. To forgive, divine.",
+        "The well bred horse ignores the barking dog.",
+        "What harms, often teaches.",
+        "The Oracle was right, you are not The One.",
+        "If you wish to be loved, love.",
+        "Only fools lend light to the sun.",
+        "There is no medicine to cure hatred.",
+        "One falsehood spoils a thousand truths.",
+        "Expect a cow where there is grass.",
+        "Every fool is pleased with his own folly.", // 1 to 30 - Negative
+        "From caring comes courage.",
+        "The greatest virtues are those useful to others.",
+        "If you can't reward, make sure to thank.",
+        "Silence is the sign of approval.",
+        "A good name is better than great riches.",
+        "As above, so below.",
+        "Rumor is not always false.",
+        "Great people have great values and great ethics.",
+        "There’s nothing more important than ethics.",
+        "Unity is power.",
+        "To understand is the base of virtue.",
+        "Virtue is the truest nobility.",
+        "Great acts are made up of small deeds.",
+        "Nothing strengthens authority as much as silence.",
+        "The way to predict the future is to shape it.",
+        "The future belongs to those who shape it today.",
+        "The time is always right to do what is right.",
+        "Culture resides in the hearts of the people.",
+        "If you are charitable, you are rich.",
+        "The wisest is the one who can forgive.",
+        "Don't waste time throwing ice cubes at the sun.",
+        "Thank who gives you and give who thanks you.",
+        "A ripe mango has to be eaten slowly.",
+        "I am what I am because of who we all are.",
+        "Wisdom does not come overnight.",
+        "To get lost is to learn the way.",
+        "He who learns, teaches.",
+        "It takes a village to raise a child.",
+        "Sticks in a bundle are unbreakable.",
+        "Many hands make light work.",
+        "What you give you get, ten times over.",
+        "As a man thinks in his heart, so is he.",
+        "Riches add to the house, virtues to the man.",
+        "Speak the truth, but leave immediately after.",
+        "Don't ask for permission, ask for forgiveness."]
+}
