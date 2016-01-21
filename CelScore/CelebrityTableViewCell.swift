459d6b5e89c8e26a871317d26d6356035a6af5a3
Gareth Mensah <gmensah@gmail.com>
2016-01-20 16:55:23 -0800

    Quick Fix.

--- CelScore/CelebrityTableViewCell.swift
+++ CelScore/CelebrityTableViewCell.swift
@@ -14,34 +14,44 @@ final class CelebrityTableViewCell: ASCellNode {
     //MARK: Properties
     let celebST: CelebrityStruct
     let nameNode: ASTextNode
-    let pictureNode: ASImageNode //TODO: ASMultiplexImageNode/ASNetworkImageNode/ASLazyImageNode
+    let profilePicNode: ASImageNode //TODO: ASMultiplexImageNode/ASNetworkImageNode/ASLazyImageNode
     let ratingsNode: ASImageNode
     let followSwitch: UISwitch
     
     //MARK: Initializer
     init(celebrityStruct: CelebrityStruct) {
         self.celebST = celebrityStruct
+        
         self.nameNode = ASTextNode()
-        self.pictureNode = ASImageNode()
-        self.ratingsNode = ASImageNode()
+        self.nameNode.attributedString = NSMutableAttributedString(string:"\(celebST.nickname)")
+        self.nameNode.placeholderEnabled = true;
         self.nameNode.layerBacked = true
-        self.pictureNode.layerBacked = true
+        
+        self.profilePicNode = ASImageNode()
+        self.profilePicNode.layerBacked = true
+//        self.profilePicNode.imageModificationBlock = {
+//            input in return input
+//        }
+        
+        self.ratingsNode = ASImageNode()
         self.ratingsNode.layerBacked = true
+        
         self.followSwitch = UISwitch()
         
         super.init()
         
         self.backgroundColor = UIColor.whiteColor()
-        self.nameNode.attributedString = NSMutableAttributedString(string:"\(celebST.nickname)")
-        self.nameNode.placeholderEnabled = true;
+        self.addSubnode(self.profilePicNode)
         self.addSubnode(self.nameNode)
+        self.addSubnode(self.ratingsNode)
+        //self.view.addSubview(self.followSwitch)
     }
     
     //MARK: Methods
     override func layoutSpecThatFits(constrainedSize: ASSizeRange) -> ASLayoutSpec {
         return ASBackgroundLayoutSpec(
             child: ASInsetLayoutSpec(
-                insets: UIEdgeInsetsMake(15, 15, 15, 15),
+                insets: UIEdgeInsetsMake(Constants.cellPadding, Constants.cellPadding, Constants.cellPadding, Constants.cellPadding),
                 child: self.nameNode),
             background: nil)
     }
