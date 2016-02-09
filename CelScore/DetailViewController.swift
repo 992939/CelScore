687630a2131cb41d7751fa50b795d56c8f5e921c
Gareth Mensah <gmensah@gmail.com>
2016-02-08 16:05:51 -0800

    IQuick Fix.

--- CelScore/DetailViewController.swift
+++ CelScore/DetailViewController.swift
@@ -35,9 +35,6 @@ final class DetailViewController: ASViewController, SMSegmentViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let maxWidth = Constants.kScreenWidth - 2 * Constants.kCellPadding
-        
-        //navigationBarView
         let backButton: FlatButton = FlatButton()
         backButton.pulseColor = MaterialColor.white
         backButton.pulseScale = false
@@ -51,14 +48,55 @@ final class DetailViewController: ASViewController, SMSegmentViewDelegate {
         navigationBarView.image = UIImage(named: "demo-cover-photo-2")
         navigationBarView.contentMode = .ScaleAspectFit
         
-        //topView
-        let topView: MaterialPulseView = MaterialPulseView(frame: CGRect(
-            x: Constants.kCellPadding,
-            y: navigationBarView.bottom + 10,
-            width: maxWidth,
-            height: 170))
-        topView.depth = .Depth2
+        let maxWidth = Constants.kScreenWidth - 2 * Constants.kCellPadding
         
+        let topViewFrame = CGRect(x: Constants.kCellPadding, y: navigationBarView.bottom + 10, width: maxWidth, height: 170)
+        let topView: MaterialPulseView = getTopView(topViewFrame)
+        
+        let segmentView = SMSegmentView(frame: CGRect(x: Constants.kCellPadding, y: topView.bottom + Constants.kCellPadding, width: maxWidth, height: 40),
+            separatorColour: MaterialColor.grey.lighten3, separatorWidth: 1,
+            segmentProperties:[keySegmentTitleFont: UIFont.systemFontOfSize(12),
+                keySegmentOnSelectionColour: Constants.kMainGreenColor,
+                keySegmentOffSelectionColour: MaterialColor.grey.lighten5,
+                keyContentVerticalMargin: 5])
+        segmentView.addSegmentWithTitle("CelScore", onSelectionImage: UIImage(named: "target_light"), offSelectionImage: UIImage(named: "target"))
+        segmentView.addSegmentWithTitle("Info", onSelectionImage: UIImage(named: "handbag_light"), offSelectionImage: UIImage(named: "handbag"))
+        segmentView.addSegmentWithTitle("Votes", onSelectionImage: UIImage(named: "globe_light"), offSelectionImage: UIImage(named: "globe"))
+        segmentView.selectSegmentAtIndex(0)
+        segmentView.layer.cornerRadius = 5.0
+        segmentView.layer.borderColor = MaterialColor.blueGrey.darken4.CGColor
+        segmentView.layer.borderWidth = 1.0
+        segmentView.delegate = self
+        
+        self.bottomViewFrame = CGRect(
+            x: Constants.kCellPadding,
+            y: segmentView.bottom + Constants.kCellPadding,
+            width: maxWidth,
+            height: Constants.kScreenHeight - (segmentView.bottom + 2 * Constants.kCellPadding))
+        let bottomVC = CelScoreViewController(celebrityST: self.celebST, frame: self.bottomViewFrame!)
+        
+        self.view.addSubview(navigationBarView)
+        self.view.sendSubviewToBack(navigationBarView)
+        self.view.addSubview(topView)
+        self.view.addSubview(segmentView)
+        self.view.addSubview(bottomVC.view)
+        self.view.backgroundColor = MaterialColor.blueGrey.darken4
+        
+        CelScoreViewModel().getFortuneCookieSignal(cookieType: .Positive).start()
+    }
+    
+    func backAction() { self.dismissViewControllerAnimated(true, completion: nil) }
+    
+    func shareVote() {
+        CelScoreViewModel().shareVoteOnSignal(socialNetwork: .Facebook)
+            .on(next: { socialVC in
+                UIApplication.sharedApplication().keyWindow!.rootViewController!.presentViewController(socialVC, animated: true, completion: nil)
+            })
+            .start()
+    }
+    
+    func getTopView(frame: CGRect) -> MaterialPulseView {
+        let topView = MaterialPulseView(frame: frame)
         let viewMaxWidth = topView.width - 2 * Constants.kCellPadding
         
         let celebPicNode = ASNetworkImageNode(webImage: ())
@@ -87,47 +125,8 @@ final class DetailViewController: ASViewController, SMSegmentViewDelegate {
         topView.addSubview(celebPicNode.view)
         topView.addSubview(nameLabel)
         topView.addSubview(roleLabel)
-        
-        let segmentView = SMSegmentView(frame: CGRect(x: Constants.kCellPadding, y: topView.bottom + Constants.kCellPadding, width: maxWidth, height: 40),
-            separatorColour: MaterialColor.grey.lighten3, separatorWidth: 1,
-            segmentProperties:[keySegmentTitleFont: UIFont.systemFontOfSize(12),
-                keySegmentOnSelectionColour: Constants.kMainGreenColor,
-                keySegmentOffSelectionColour: MaterialColor.grey.lighten5,
-                keyContentVerticalMargin: 5])
-        segmentView.addSegmentWithTitle("CelScore", onSelectionImage: UIImage(named: "target_light"), offSelectionImage: UIImage(named: "target"))
-        segmentView.addSegmentWithTitle("Info", onSelectionImage: UIImage(named: "handbag_light"), offSelectionImage: UIImage(named: "handbag"))
-        segmentView.addSegmentWithTitle("Votes", onSelectionImage: UIImage(named: "globe_light"), offSelectionImage: UIImage(named: "globe"))
-        segmentView.selectSegmentAtIndex(0)
-        segmentView.layer.cornerRadius = 5.0
-        segmentView.layer.borderColor = MaterialColor.blueGrey.darken4.CGColor
-        segmentView.layer.borderWidth = 1.0
-        segmentView.delegate = self
-        
-        self.bottomViewFrame = CGRect(
-            x: Constants.kCellPadding,
-            y: segmentView.bottom + Constants.kCellPadding,
-            width: maxWidth,
-            height: Constants.kScreenHeight - (segmentView.bottom + 2 * Constants.kCellPadding))
-        let bottomVC = CelScoreViewController(celebrityST: self.celebST, frame: self.bottomViewFrame!)
-        
-        self.view.backgroundColor = MaterialColor.blueGrey.darken4
-        self.view.addSubview(navigationBarView)
-        self.view.sendSubviewToBack(navigationBarView)
-        self.view.addSubview(topView)
-        self.view.addSubview(segmentView)
-        self.view.addSubview(bottomVC.view)
-        
-        CelScoreViewModel().getFortuneCookieSignal(cookieType: .Positive).start()
-    }
-    
-    func backAction() { self.dismissViewControllerAnimated(true, completion: nil) }
-    
-    func shareVote() {
-        CelScoreViewModel().shareVoteOnSignal(socialNetwork: .Facebook)
-            .on(next: { socialVC in
-                UIApplication.sharedApplication().keyWindow!.rootViewController!.presentViewController(socialVC, animated: true, completion: nil)
-            })
-            .start()
+        topView.depth = .Depth2
+        return topView
     }
     
     //MARK: SMSegmentViewDelegate
