383808dcbdfb65d3adff87b922674dfea73a4e2a
Gareth Mensah <gmensah@gmail.com>
2016-02-25 08:43:51 -0800

    Quick Fix.

--- CelScore/RFExtensions.swift
+++ CelScore/RFExtensions.swift
@@ -36,6 +36,16 @@ public extension UIView {
     }
 }
 
+public extension UIButton {
+    public override func hitTest(point: CGPoint, withEvent event: UIEvent?) -> UIView? {
+        let buttonSize = self.frame.size
+        let widthToAdd = (44-buttonSize.width > 0) ? 44-buttonSize.width : 0
+        let heightToAdd = (44-buttonSize.height > 0) ? 44-buttonSize.height : 0
+        let largerFrame = CGRect(x: 0-(widthToAdd/2), y: 0-(heightToAdd/2), width: buttonSize.width+widthToAdd, height: buttonSize.height+heightToAdd)
+        return (CGRectContainsPoint(largerFrame, point)) ? self : nil
+    }
+}
+
 public extension NSDate {
 
     //MARK: Methods
