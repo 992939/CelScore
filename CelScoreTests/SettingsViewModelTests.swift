e9b8086959fa00f224acb30ec01d1d22ae03a047
Gareth Mensah <gmensah@gmail.com>
2016-04-13 19:59:58 -0700

    QUick Fix.

--- CelScoreTests/SettingsViewModelTests.swift
+++ CelScoreTests/SettingsViewModelTests.swift
@@ -158,6 +158,126 @@ class SettingsViewModelTests: XCTestCase {
             XCTAssert((value as Any) is Bool); expectation.fulfill() }
         waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("FirstTrollWarning error: \(error)") } }
     }
+    
+    func testUpdateDefaultListIndex() {
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+        let expectation = expectationWithDescription("set DefaultListIndex to Int(1)")
+        SettingsViewModel().updateSettingSignal(value: Int(1), settingType: .DefaultListIndex).startWithNext { settings in
+            XCTAssertEqual(settings.defaultListIndex, Int(1)); expectation.fulfill() }
+       waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("set DefaultListIndex error: \(error)") } }
+    }
+    
+    func testUpdateLoginTypeIndex() {
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+        let expectation = expectationWithDescription("set LoginTypeIndex to Int(1)")
+        SettingsViewModel().updateSettingSignal(value: Int(1), settingType: .LoginTypeIndex).startWithNext { settings in
+            XCTAssertEqual(settings.loginTypeIndex, Int(1)); expectation.fulfill() }
+        waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("set LoginTypeIndex error: \(error)") } }
+    }
+    
+    func testUpdatePublicService() {
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+        let expectation = expectationWithDescription("set PublicService to true")
+        SettingsViewModel().updateSettingSignal(value: true, settingType: .PublicService).startWithNext { settings in
+            XCTAssertEqual(settings.publicService, true); expectation.fulfill() }
+        waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("set PublicService error: \(error)") } }
+    }
+    
+    func testUpdateConsensusBuilding() {
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+        let expectation = expectationWithDescription("set ConsensusBuilding to true")
+        SettingsViewModel().updateSettingSignal(value: true, settingType: .ConsensusBuilding).startWithNext { settings in
+            XCTAssertEqual(settings.consensusBuilding, true); expectation.fulfill() }
+        waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("set ConsensusBuilding error: \(error)") } }
+    }
+    
+    func testUpdateFirstLaunch() {
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+        let expectation = expectationWithDescription("set FirstLaunch to false")
+        SettingsViewModel().updateSettingSignal(value: true, settingType: .FirstLaunch).startWithNext { settings in
+            XCTAssertEqual(settings.isFirstLaunch, false); expectation.fulfill() }
+        waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("set FirstLaunch error: \(error)") } }
+    }
+    
+    func testUpdateFirstConsensus() {
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+        let expectation = expectationWithDescription("set FirstConsensus to false")
+        SettingsViewModel().updateSettingSignal(value: true, settingType: .FirstConsensus).startWithNext { settings in
+            XCTAssertEqual(settings.isFirstConsensus, false); expectation.fulfill() }
+        waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("set FirstConsensus error: \(error)") } }
+    }
+    
+    func testUpdateFirstPublic() {
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+        let expectation = expectationWithDescription("set FirstPublic to false")
+        SettingsViewModel().updateSettingSignal(value: true, settingType: .FirstPublic).startWithNext { settings in
+            XCTAssertEqual(settings.isFirstPublic, false); expectation.fulfill() }
+        waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("set FirstPublic error: \(error)") } }
+    }
+    
+    func testUpdateFirstFollow() {
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+        let expectation = expectationWithDescription("set FirstFollow to false")
+        SettingsViewModel().updateSettingSignal(value: true, settingType: .FirstFollow).startWithNext { settings in
+            XCTAssertEqual(settings.isFirstFollow, false); expectation.fulfill() }
+        waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("set FirstFollow error: \(error)") } }
+    }
+    
+    func testUpdateFirstStars() {
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+        let expectation = expectationWithDescription("set FirstFollow to false")
+        SettingsViewModel().updateSettingSignal(value: true, settingType: .FirstStars).startWithNext { settings in
+            XCTAssertEqual(settings.isFirstStars, false); expectation.fulfill() }
+        waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("set FirstStars error: \(error)") } }
+    }
+    
+    func testUpdateFirstNegative() {
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+        let expectation = expectationWithDescription("set FirstNegative to false")
+        SettingsViewModel().updateSettingSignal(value: true, settingType: .FirstNegative).startWithNext { settings in
+            XCTAssertEqual(settings.isFirstNegative, false); expectation.fulfill() }
+        waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("set FirstNegative error: \(error)") } }
+    }
+    
+    func testUpdateFirstInterest() {
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+        let expectation = expectationWithDescription("set FirstInterest to false")
+        SettingsViewModel().updateSettingSignal(value: true, settingType: .FirstInterest).startWithNext { settings in
+            XCTAssertEqual(settings.isFirstInterest, false); expectation.fulfill() }
+        waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("set FirstInterest error: \(error)") } }
+    }
+    
+    func testUpdateFirstCompleted() {
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+        let expectation = expectationWithDescription("set FirstCompleted to false")
+        SettingsViewModel().updateSettingSignal(value: true, settingType: .FirstCompleted).startWithNext { settings in
+            XCTAssertEqual(settings.isFirstCompleted, false); expectation.fulfill() }
+        waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("set FirstCompleted error: \(error)") } }
+    }
+    
+    func testUpdateFirstVoteDisable() {
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+        let expectation = expectationWithDescription("set FirstVoteDisable to false")
+        SettingsViewModel().updateSettingSignal(value: true, settingType: .FirstVoteDisable).startWithNext { settings in
+            XCTAssertEqual(settings.isFirstVoteDisabled, false); expectation.fulfill() }
+        waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("set FirstVoteDisable error: \(error)") } }
+    }
+    
+    func testUpdateFirstSocialDisable() {
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+        let expectation = expectationWithDescription("set FirstSocialDisable to false")
+        SettingsViewModel().updateSettingSignal(value: true, settingType: .FirstSocialDisable).startWithNext { settings in
+            XCTAssertEqual(settings.isFirstSocialDisabled, false); expectation.fulfill() }
+        waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("set FirstSocialDisable error: \(error)") } }
+    }
+    
+    func testUpdateFirstTrollWarning() {
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+        let expectation = expectationWithDescription("set FirstTrollWarning to false")
+        SettingsViewModel().updateSettingSignal(value: true, settingType: .FirstTrollWarning).startWithNext { settings in
+            XCTAssertEqual(settings.isFirstTrollWarning, false); expectation.fulfill() }
+        waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("set FirstTrollWarning error: \(error)") } }
+    }
 }
 
 
