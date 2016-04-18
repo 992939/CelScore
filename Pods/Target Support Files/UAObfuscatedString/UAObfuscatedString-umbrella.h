0579f9a5f3444c2daabbe05320731d374a8f7582
Gareth Mensah <gmensah@gmail.com>
2016-04-18 00:28:45 -0700

    QUick Fix Again!

---
+++ Pods/Target Support Files/UAObfuscatedString/UAObfuscatedString-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "UAObfuscatedString.h"
+
+FOUNDATION_EXPORT double UAObfuscatedStringVersionNumber;
+FOUNDATION_EXPORT const unsigned char UAObfuscatedStringVersionString[];
+
