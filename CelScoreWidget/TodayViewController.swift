1ff2348c3dedec05d912772f8e0242b7a29941bc
Gareth Mensah <gmensah@gmail.com>
2016-04-18 17:54:24 -0700

    QUick FIx.

--- CelScoreWidget/TodayViewController.swift
+++ CelScoreWidget/TodayViewController.swift
@@ -59,10 +59,6 @@ final class TodayViewController: UITableViewController, NCWidgetProviding {
     }
     
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
         completionHandler(NCUpdateResult.NewData)
     }
     
