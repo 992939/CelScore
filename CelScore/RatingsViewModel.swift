bb0675deef81abc1d27c36c1196c8dd251760a23
Gareth Mensah <gmensah@gmail.com>
2016-04-20 16:12:22 -0700

    QUick FIx.

--- CelScore/RatingsViewModel.swift
+++ CelScore/RatingsViewModel.swift
@@ -83,7 +83,6 @@ struct RatingsViewModel {
             else if newRatings!.totalVotes == 0 { newRatings!.forEach({ rating in newRatings![rating] = 0 }) }
             realm.add(newRatings!, update: true)
             try! realm.commitWrite()
-            print("clean")
             observer.sendNext(newRatings!)
             observer.sendCompleted()
         }
