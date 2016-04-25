f5ec18071c9c5aef7412921c7fa2b0149b1e6d41
Gareth Mensah <gmensah@gmail.com>
2016-04-25 01:01:02 -0700

    QUick Fix.

--- CelScore/RFExtensions.swift
+++ CelScore/RFExtensions.swift
@@ -268,7 +268,7 @@ extension UIDevice {
         let height: CGFloat
         switch Constants.kScreenHeight {
         case Constants.kIPhone4_height: height = 102.5
-        case Constants.kIPhone5_height: height = 80
+        case Constants.kIPhone5_height: height = 75
         case Constants.kIPhone6_height: height = 80
         default: height = 75
         }
