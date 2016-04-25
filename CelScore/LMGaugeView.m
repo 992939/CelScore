362357a0000e89b7f62ce75e1b65fa24411a7aa5
Gareth Mensah <gmensah@gmail.com>
2016-04-25 00:30:59 -0700

    QUick Fix.

--- CelScore/LMGaugeView.m
+++ CelScore/LMGaugeView.m
@@ -33,7 +33,7 @@
 #define kDefaultValueFont                       [UIFont fontWithName:@"HelveticaNeue-CondensedBold" size:80]
 #define kDefaultValueTextColor                  [UIColor colorWithWhite:0.1 alpha:1]
 
-#define kDefaultUnitOfMeasurement               @"%CBV"
+#define kDefaultUnitOfMeasurement               @"%/vol"
 #define kDefaultUnitOfMeasurementFont           [UIFont fontWithName:@"HelveticaNeue-CondensedBold" size:16]
 #define kDefaultUnitOfMeasurementTextColor      [UIColor colorWithWhite:0.3 alpha:1]
 
