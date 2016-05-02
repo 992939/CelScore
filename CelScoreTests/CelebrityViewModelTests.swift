26202de1b437719a09b26469713add8a5d8a76e8
Gareth Mensah <gmensah@gmail.com>
2016-05-02 15:47:43 -0700

    QUick Fix.

--- CelScoreTests/CelebrityViewModelTests.swift
+++ CelScoreTests/CelebrityViewModelTests.swift
@@ -19,7 +19,9 @@ class CelebrityViewModelTests: XCTestCase {
         realm.beginWrite()
         let celeb = CelebrityModel()
         celeb.id = "0001"
+        celeb.picture3x = "https://imageURL.com/test@3x.png"
         celeb.isFollowed = true
+        celeb.isNew = true
         realm.add(celeb, update: true)
         try! realm.commitWrite()
     }
@@ -45,6 +47,21 @@ class CelebrityViewModelTests: XCTestCase {
         waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("followCebritySignal error: \(error)") } }
     }
     
+    func testGetNewCelebsSignal() {
+        let expectation = expectationWithDescription("getNewCelebsSignal callback")
+        CelScoreViewModel().getNewCelebsSignal()
+            .on(next: { celebInfo in
+            XCTAssert(celebInfo.text.characters.count > 0, "NewCelebInfo text String must be > 0")
+            XCTAssert(celebInfo.image.characters.count > 0, "NewCelebInfo image String must be > 0") })
+            .on(completed: {
+                let realm = try! Realm()
+                let celebs = realm.objects(CelebrityModel).filter("isNew = %@", true)
+                XCTAssertEqual(celebs.count, 0, "new celebs count must be 0")
+                expectation.fulfill() })
+            .start()
+        waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("getNewCelebsSignal error: \(error)") } }
+    }
+    
     func testCountFollowedCelebritiesSignal() {
         let expectation = expectationWithDescription("countFollowedCelebritiesSignal callback")
         CelebrityViewModel().countFollowedCelebritiesSignal().startWithNext { count in
@@ -59,7 +76,8 @@ class CelebrityViewModelTests: XCTestCase {
             let userDefaults: NSUserDefaults = NSUserDefaults(suiteName:"group.NotificationApp")!
             userDefaults.synchronize()
             let rowsNumber: Int = userDefaults.integerForKey("count")
-            XCTAssertEqual(rowsNumber, 1, "NSUserDefaults equals 1."); expectation.fulfill()
+            XCTAssertEqual(rowsNumber, 1, "NSUserDefaults equals 1.")
+            expectation.fulfill()
         }
         waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("testUpdateWidgetSignal error: \(error)") } }
     }
