23b621d1630f04aa589012c51ab504f17670bd1f
Gareth Mensah <gmensah@gmail.com>
2016-05-03 15:49:50 -0700

    More Unit Tests.

---
+++ CelScoreTests/CelScoreViewModelTests.swift
@@ -0,0 +1,27 @@
+//
+//  CelScoreViewModelTests.swift
+//  CelScore
+//
+//  Created by Gareth.K.Mensah on 5/3/16.
+//  Copyright © 2016 Gareth.K.Mensah. All rights reserved.
+//
+
+import XCTest
+import RealmSwift
+@testable import CelebrityScore
+
+class CelScoreViewModelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func testShareVoteOnSignal() {
+        let expectation = expectationWithDescription("shareVoteOnSignal callback")
+        CelScoreViewModel().shareVoteOnSignal(socialLogin: .Facebook, message: "message", screenshot: UIImage()).startWithNext { composer in
+            XCTAssertNotNil(composer, "SLComposeViewController must not be nil")
+            expectation.fulfill()
+        }
+        waitForExpectationsWithTimeout(1) { error in if let error = error { XCTFail("shareVoteOnSignal error: \(error)") } }
+    }
+}
