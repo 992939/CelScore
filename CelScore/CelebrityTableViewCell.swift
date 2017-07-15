7f27eb1a5978392736c639b7cac892e8bdb03e47
Gareth Mensah <gmensah@gmail.com>
2017-07-14 21:23:43 -0700

    QUick Fix.

--- CelScore/CelebrityTableViewCell.swift
+++ CelScore/CelebrityTableViewCell.swift
@@ -151,15 +151,18 @@ final class celebrityTableNodeCell: ASCellNode, BEMCheckBoxDelegate {
         middleStack.style.flexBasis = ASDimensionMake(.fraction, 0.2)
         middleStack.style.flexGrow = 1
         
+        let spacer = ASLayoutSpec()
+        spacer.style.flexGrow = 0.2
+        
         let switchStack = ASStackLayoutSpec(
             direction: .vertical,
-            spacing: Constants.kPadding/4,
-            justifyContent: .end,
-            alignItems: .end,
-            children: [self.switchNode])
+            spacing: Constants.kPadding,
+            justifyContent: .start,
+            alignItems: .start,
+            children: [spacer, self.switchNode])
         switchStack.style.flexBasis = ASDimensionMake(.fraction, 1)
-        switchStack.style.ascender = 10.0
-        switchStack.style.flexGrow = 1
+        switchStack.style.flexGrow = 0.8
+        
         
         let bottomRightStack = ASStackLayoutSpec(
             direction: .horizontal,
